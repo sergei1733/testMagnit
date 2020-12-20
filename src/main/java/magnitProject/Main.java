65f753a05f02a487d8b4979d0ebccaa4574e0461
sergei1733 <sergei1733@yandex.ru>
2020-12-21 01:28:21 +0500

    "keyboard input"

--- src/main/java/magnitProject/Main.java
+++ src/main/java/magnitProject/Main.java
@@ -13,24 +13,19 @@ import java.util.logging.Logger;
 public class Main {
     private static Logger log = Logger.getLogger(Main.class.getName());
     public static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
         Long numberRecords;
 
         System.out.print("Enter the number of records ");
         numberRecords = in.nextLong();
-
         Dao dao = new Dao();
-        dao.setData(numberRecords);              //заполнение таблицы бд
-
+        dao.setData(numberRecords);                                 //заполнение таблицы бд
         CreateDocument crDoc = new CreateDocument();
-
         try {
-            Document doc = crDoc.createDoc();            //создание документа
-
-            Element entries = crDoc.createXMLHead(doc);  //заполнение шапки документа
-
-            List<Long> list = dao.getData();   //получение записей из таблицы
-
+            Document doc = crDoc.createDoc();                       //создание документа
+            Element entries = crDoc.createXMLHead(doc);             //заполнение шапки документа
+            List<Long> list = dao.getData();                        //получение записей из таблицы
             for (Long field : list) {
                 crDoc.fillingOutTheDocument(doc, entries, field);   //заполнение тела xml документа
             }
@@ -38,13 +33,9 @@ public class Main {
             crDoc.saveTransformerDoc();
             ReadingXMLDocument read = new ReadingXMLDocument();
             read.parseXML();
-
         } catch (Exception e) {
             log.info("Program execution error!");
             throw e;
         }
-
-
     }
-
 }
